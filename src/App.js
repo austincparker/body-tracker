af5901c014dbbd285b83aae761b84da3f65167c9
Austin Parker <austincparker@gmail.com>
2022-07-24 21:55:44 -0500

    installed material and added nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import Home from './views/Home';
 
 
 function App() {
   return (
+    <div className="App">
+    <Navbar />
     <Home />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <h4 className="p-3">Navbar</h4>
+  )
+}
+
+export default Navbar
